9efc7179e64208553e66cc874d89ed2b0766b202
Sanchez <asanchez386@cps.k12.il.us>
2024-10-25 08:28:59 -0500

    we started our group work

---
+++ text.py
@@ -0,0 +1,25 @@
+# newphrase = "rainstorm"
+#             # 012345678
+#             # 123456789 college board version
+# # create a new variable called shortphrase
+# #  and assign it a value by slicing
+# #  the new phrase variable by removing
+# #  the first three characters
+# #  and the last three characters
+# # the first three characters are "rai"
+# #  The last three characters are "orm"
+# #  substring(string,start,end)
+# shortphrase = newphrase[3:len(newphrase)-3]
+# # college board version [4:len(newphrase)-6]
+# print(shortphrase)
+# # explain len(newphrase) = 9-3 = 6
+# # why does it end with 6
+# # because the last index is not included
+
+# import turtle
+# t = turtle.Turtle()
+
+# r = 50
+# t.circle(r)
+x = 0
+input("guess a random number. ")
